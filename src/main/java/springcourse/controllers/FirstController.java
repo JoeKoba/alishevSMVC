30db86a225060511373c2cae968c8faf2de3d3fc
Evgeny Voinov <joekobarnd@gmail.com>
2022-12-04 20:37:29 +0300

    20

--- src/main/java/springcourse/controllers/FirstController.java
+++ src/main/java/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,14 +14,36 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println(name + surname);
+        model.addAttribute("message", "hello" + name + surname);
         return "first/hello";
     }
 
     @GetMapping("goodbye")
     public String goodbyePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a") int a,
+                             @RequestParam(value = "b") int b,
+                             @RequestParam(value = "action") String action,
+                             Model model) {
+        int res = 0;
+
+        if (action.equals("multi")) {
+            res = a * b;
+        } else if (action.equals("add")) {
+            res = a + b;
+        } else if (action.equals("sub")) {
+            res = a - b;
+        } else {
+            res = a / b;
+        }
+
+        model.addAttribute("res", res);
+        return "first/calc";
     }
 }
